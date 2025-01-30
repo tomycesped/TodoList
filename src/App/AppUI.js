441a8639fd728085d851cbd6d78bbee78d23ef8d
tomycesped <elefantinto@gmail.com>
2025-01-30 12:46:56 -0300

    Agregado de componente NoResults

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -2,14 +2,15 @@ import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodosError } from '../TodosError';
-import { TodosLoading} from '../TodosLoading';
-import { EmptyTodos } from '../EmptyTodos';import { TodoItem } from '../TodoItem';
+import { TodosLoading } from '../TodosLoading';
+import { EmptyTodos } from '../EmptyTodos';
+import { NoResults } from '../NoResults'; // Importa el nuevo componente
+import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 import { TodoContext } from '../TodoContext';
 import React from 'react';
 import { Modal } from '../Modal';
 import { TodoForm } from '../TodoForm';
-
 
 function AppUI() {
   const {
@@ -20,41 +21,52 @@ function AppUI() {
     deleteTodo,
     openModal,
     setOpenModal,
-    resetRotation
+    resetRotation,
+    searchValue, // Asegúrate de obtener searchValue del contexto
   } = React.useContext(TodoContext);
-    return (
-        <> 
-          <TodoCounter  />
-          <TodoSearch />
 
-            <TodoList>
-            {loading && 
-            <>
-            <TodosLoading />
-            <TodosLoading />
-            <TodosLoading />
-            </> }
-            {error && <TodosError />}
-            {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
-  
-            {searchedTodos.map(todo => (
-              <TodoItem
-               key={todo.text}
-                text={todo.text} 
-                completed={todo.completed} 
-                onComplete={() => completeTodo(todo.text)} 
-                onDelete={() => deleteTodo(todo.text)}/>
-            ))}
-            </TodoList>
-        <CreateTodoButton setOpenModal={setOpenModal} resetRotation={resetRotation}/>
+  return (
+    <> 
+      <TodoCounter />
+      <TodoSearch />
 
-        {openModal && (
-        <Modal>
-          <TodoForm resetRotation={resetRotation}/>
-        </Modal>
+      <TodoList>
+        {loading && (
+          <>
+            <TodosLoading />
+            <TodosLoading />
+            <TodosLoading />
+          </>
         )}
-        </>
-      );
-    }
+        {error && <TodosError />}
+        
+        {!loading && searchedTodos.length === 0 && !searchValue && (
+          <EmptyTodos />
+        )}
+        {!loading && searchedTodos.length === 0 && searchValue && (
+          <NoResults />
+        )}
 
-    export {AppUI};
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton setOpenModal={setOpenModal} resetRotation={resetRotation} />
+
+      {openModal && (
+        <Modal>
+          <TodoForm resetRotation={resetRotation} />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export { AppUI };
---
+++ src/NoResults/index.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import "./NoResults.css"
+
+function NoResults() {
+  return (
+    <p className="NoResults-p">No hay tareas que coincidan 🤔</p>
+  );
+}
+
+export { NoResults };
